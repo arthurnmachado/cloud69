cf53918dd2d1d3924aaf9d05be2302c382e6b52d
arthurnagasemachado <arthurnagase@aluno.escola.sescms.com.br>
2020-11-13 15:22:38 -0400

    Versão 1.07

---
+++ Assets/Scripts/FadeAudioMixer.cs
@@ -0,0 +1,58 @@
+﻿// =============================================================================
+//                            Criado por John of gamedevbeginner
+// =============================================================================
+//
+// Fonte: https://gamedevbeginner.com/how-to-fade-audio-in-unity-i-tested-every-method-this-ones-the-best/
+//
+// =============================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public static class FadeAudioMixer
+{
+
+    public static IEnumerator StartFade(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume, float fadeOutWait = 0f)
+    {
+
+        yield return new WaitForSeconds(fadeOutWait);
+
+        float currentTime = 0;
+        float currentVol;
+
+        // Para fazermos o fadeOut
+
+        // Pega o volume atual do Audiomixer
+        // Variaveis out sao variaveis de saida
+        // Sao modificadas dentro do escopo da funcao
+        audioMixer.GetFloat(exposedParam, out currentVol);
+
+        // currentVol =  10^(currentVol/20)
+        currentVol = Mathf.Pow(10, currentVol / 20);
+        
+        // limita o valor destino entre 0 e 1
+        float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
+
+        // Verifica durante a duracao, diminuindo o volume
+        while (currentTime < duration)
+        {
+            // Soma quanto tempo passou
+            currentTime += Time.deltaTime;
+
+            // Interpola os valores currentVol e targetValue usando currentTime / duration
+            // Isto eh, supondo valores 0(origem) e 10(destino) 
+            // Se passarmos 0.5, como valor intermediario
+            // o Lerp retorna a interpolação linear, ou seja, 5
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
+
+            // Por fim, atribua ao AudioMixer esse volume
+            audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
+
+            // Retorna nulo para poder ficar neste no, e retoma-lo no proximo frame
+            yield return null;
+        }
+        yield break;
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioSource backgroundAudioSrc, defaultShotAudioSrc;
+    public float fadeTime = 0.5f,
+                 defaultBackgroundVolume = 1f;
+
+    private static SoundManager soundManager;
+    private bool hasBackgroundAudioSrc, hasOneShotAudioSrc;
+    private AudioMixer backgroundMixer;
+
+    public static SoundManager instance
+    {
+        get
+        {
+
+            if (!soundManager)
+            {
+                soundManager = FindObjectOfType<SoundManager>() as SoundManager; 
+
+                if (soundManager is null)
+                {
+                    Debug.Log("There needs to be one active SoundManager script on a GameObject in your scene.");
+                }
+                else
+                {
+                    soundManager.Init();
+                }
+
+                
+            }
+
+            return soundManager;
+        }
+    }
+
+    void Awake()
+    {
+        if (soundManager)
+            Destroy(gameObject);
+        else if (SoundManager.instance)
+        {
+            //pass
+        }
+           
+    }
+
+    private void Init()
+    {
+        // Temos um audio source de background?
+        hasBackgroundAudioSrc = backgroundAudioSrc != null;
+        // Temos um audio source de Sons unicos?
+        hasOneShotAudioSrc = defaultShotAudioSrc != null;
+
+        if (hasBackgroundAudioSrc)
+        {
+            backgroundMixer = backgroundAudioSrc.outputAudioMixerGroup.audioMixer;
+            bool returnB = backgroundMixer.SetFloat("BackgroundMusicVolume" ,-80f);
+        }
+        
+    }
+
+    public void PlaySound(AudioClip sound)
+    {
+
+        if (instance.defaultShotAudioSrc.isPlaying)
+        {
+            instance.defaultShotAudioSrc.Stop();
+            instance.StopAllCoroutines();
+        }
+
+        if (instance.hasBackgroundAudioSrc)
+        {
+            // Tem musica de fundo
+            instance.defaultShotAudioSrc.clip = sound;
+            instance.defaultShotAudioSrc.PlayDelayed(fadeTime);
+
+            // Criar uma rotina para executar o fadeIn e FadeOut
+            StartCoroutine(FadeAudioMixer.StartFade(instance.backgroundMixer, "BackgroundMusicVolume", instance.fadeTime, 0f));
+            StartCoroutine(FadeAudioMixer.StartFade(instance.backgroundMixer, "BackgroundMusicVolume", instance.fadeTime, 1f, instance.fadeTime + sound.length));
+        }
+        else
+        {
+            // Nao tem musica de fundo
+            instance.defaultShotAudioSrc.PlayOneShot(sound);
+        }
+
+    }
+
+}
